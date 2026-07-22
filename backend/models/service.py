04015a05d6c07d9a043c5daca8e26ee2d9d7d9d3
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-22 17:48:56 -0300

    feat: add service model

--- backend/models/service.py
+++ backend/models/service.py
@@ -1,8 +1,18 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 from datetime import datetime
-from sqlalchemy import func
+from sqlalchemy import func, ForeignKey, Enum as SQLEnum
+from core.enums import ServiceStatus
 
 class Service(Base):
     __tablename__ = "services"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    landlord_id: Mapped[int] = mapped_column(ForeignKey("landlords.id"))
+    property_id: Mapped[int] = mapped_column(ForeignKey("properties.id"))
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    value: Mapped[float] = mapped_column()
+    status: Mapped[ServiceStatus] = mapped_column(SQLEnum(ServiceStatus)) #enum
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
